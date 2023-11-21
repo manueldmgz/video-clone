2ddb1ac9a051af93c26243eb215a793a03e5eb5f
José Manuel Domínguez E <josemanueldomingueze@gmail.com>
2023-11-20 22:52:13 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/video-clone/",
+  base: "/",
   tesnpt: {
     globals: true,
     environment: 'jsdom',
